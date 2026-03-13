77691c8d0fe2211370609b74df388f097c404202
ravneet123209 <harpreetrandhawa70777@gmail.com>
2026-03-14 00:50:15 +0100

    Task 1 — Create a Book Object

---
+++ week4_homework.js
@@ -0,0 +1,11 @@
+//Task 1 — Create a Book Object
+
+let book = {
+  title: "Harry Potter",
+  author: "J.k rollin",
+  year: 1955,
+  rating : 5 ,
+  isAvailable: true
+};
+console.log(`The book named as ${book.title} was written by ${book.author} in year ${book.year} having rating of ${book.rating}, is available = ${book.isAvailable}`);
+
