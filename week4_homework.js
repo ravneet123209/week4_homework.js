977b9521060c0bf48cd051d5ebb07e17f35d1fd7
ravneet123209 <harpreetrandhawa70777@gmail.com>
2026-03-14 01:25:29 +0100

    Bonus

--- week4_homework.js
+++ week4_homework.js
@@ -1,4 +1,6 @@
+/////////////
 //Task 1 — Create a Book Object
+////////////
 
 let book = {
   title: "Harry Potter",
@@ -12,8 +14,9 @@ let book = {
 
 console.log(`The book named as ${book.title} was written by ${book.author} in year ${book.year} having rating of ${book.rating}, is available = ${book.isAvailable}`);
 
-
+///////////
 //Task 2 — Library Array
+//////////
 
 let library = [
   { title: "Book 1", author: "Author 1", year: 2000, isAvailable: true },
@@ -37,9 +40,9 @@ console.log("number of books available= ",availableBook);
 
 console.log("Total number of books = ",countOfBook);
 
-
+/////////
 //Task 3 — Function: Find Book
-
+////////
 const books = [
   { title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 },
   { title: "1984", author: "George Orwell", year: 1949 },
@@ -58,3 +61,41 @@ function findBook(title) {
 console.log(findBook("1984"));
 
 console.log(findBook("The Alchemist"));
+
+/////////
+//Task 4 — Calculate Average Publication Year
+////////
+
+let total = 0;
+
+for (let i = 0; i < library.length; i++) {
+  total += library[i].year;
+}
+
+let averageYear = total / library.length;
+
+console.log("Average publication year:", averageYear);
+
+////////////////
+//Bonus
+//////////////
+
+let library1 = [
+  { title: "Book 1", author: "Author 1", year: 2000,rating : 4, isAvailable: true },
+  { title: "Book 2", author: "Author 2", year: 2010,rating : 3, isAvailable: false },
+  { title: "Book 3", author: "Author 3", year: 2005,rating : 5, isAvailable: false }
+];
+
+function findHighestRatedBook() {
+  let highest = library1[0];   // assume first book is highest
+
+  for (let i = 1; i < library1.length; i++) {
+    if (library1[i].rating > highest.rating) {
+      highest = library1[i];
+    }
+  }
+
+  return highest;
+}
+
+console.log("Highest rated book:", findHighestRatedBook());
